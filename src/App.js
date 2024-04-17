780151be1243d489e6f82cacb875f716e2441acd
Starry-coder <aarryyaann1502051@gmail.com>
2024-04-17 19:46:12 +0530

    navbar update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+const Header = styled.header`
+  background-color: black;
+  min-height: 15vh;
+  display: flex;
+  padding-left: 40px;
+  flex-direction: column;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  font-weight: bold;
+  box-shadow: 0 3px 3px 0 #555
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+        CookingGen
+      </Header>
     </div>
   );
 }
